a72b7e1cf6b3cec3e236beeabbe039ad0e8c67fb
afzytk <afzytk@gmail.com>
2026-01-29 10:21:17 +0530

    setup tailwind css and shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import { Button } from "@/components/ui/button"
 
 const App = () => {
   return(
   <div>
-    <h1>Job Portal</h1>
+   <Button variant="destructive">Button</Button>
   </div>
   )
 }
