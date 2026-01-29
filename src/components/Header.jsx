d38cadf73b59708edb75fcff44963defcc0498fd
afzytk <afzytk@gmail.com>
2026-01-29 23:13:41 +0530

    Setup Clerk for authentication

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 const Header = () => {
   return (
@@ -9,7 +15,13 @@ const Header = () => {
           <img src="/logo.png" alt="jobvio log" className="h-50" />
         </Link>
 
-        <Button variant="outline">Login</Button>
+        {/* <Button variant="outline">Login</Button> */}
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </nav>
     </>
   );
