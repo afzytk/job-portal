422a1c3b5070ac048a8bbf1c9c05a9f5c066ba3a
afzytk <afzytk@gmail.com>
2026-01-29 21:22:17 +0530

    Add Header component with logo

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,13 +1,17 @@
-import { Outlet } from "react-router-dom"
-
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 const AppLayout = () => {
   return (
     <div>
       <div className="grid-background"></div>
-            <Outlet />
+      <main className="min-h-screen container">
+        <Header />
+        <Outlet />
+      </main>
+      <div className="p-10 text-center bg-gray-800 mt-10">Made with React</div>
     </div>
-  )
-}
+  );
+};
 
-export default AppLayout
+export default AppLayout;
